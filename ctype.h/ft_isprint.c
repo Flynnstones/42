bb1bf1c7e0389dc598f6c8ce8256c0c0fedf6c95
Flynnstones <filocasoni1994@gmail.com>
2024-04-26 12:19:54 +0200

    Updated the ft_isprint.c file in ctype.h folder

--- ctype.h/ft_isprint.c
+++ ctype.h/ft_isprint.c
@@ -8,6 +8,12 @@ int main(int argc, char *argv[])
         printf("Il carattere inserito non è stampabile");
     else
         printf("Il carattere inserito è stampabile");
+    return 0;
+}
 
+int ft_isprint(int c)
+{
+    if (!(c >= 32 && c <= 126))
+        return 1;
     return 0;
 }
