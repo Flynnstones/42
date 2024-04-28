f0360fa0ae98c1afe1707e03bb2294af7bf4adc9
Flynnstones <filocasoni1994@gmail.com>
2024-04-28 14:50:55 +0200

    Updated ft_isdigit.C

--- ctype.h/ft_isdigit.c
+++ ctype.h/ft_isdigit.c
@@ -1,17 +1,3 @@
-#include <stdio.h>
-
-int main (int argc, char *argv[])
-{
-    char carattere = argv[1][0];
-
-    if(carattere >= 0 && carattere <= 9)
-        printf("il carattere inserito è un numero");
-    else
-        printf("il carattere inserito non è un numero");
-    
-    return 0;
-}
-
 int ft_isdigit(int c)
 {
     if (c >= 0 && c <= 9)
