b9cc679ad37a5c0eb7dd362f5571e280478f7a6f
Flynnstones <filocasoni1994@gmail.com>
2024-04-30 23:17:52 +0200

    Sfoltimento codice

--- linked_list functions/union_vs_struct.c
+++ linked_list functions/union_vs_struct.c
@@ -3,23 +3,21 @@
 #include <stdlib.h>
 
 typedef union Data {
-    int i; // 4 bytes
-    float f; // 4 bytes
-    char str[20]; // 20 bytes
-} data; // 20 bytes
+    int i;               // 4 bytes
+    float f;            // 4 bytes
+    char str[20];      // 20 bytes
+} data;               // 20 bytes
 
 typedef struct{
-    union Data *data; // 20 bytes
-    int type; // 4 bytes
-} Data; // 24 bytes
+    union Data *data;   // 20 bytes
+    int type;          // 4 bytes
+} Data;               // 24 bytes
 
 int main() {
     data data;
-    Data data2; // Data *data2;
+    Data data2;
     int type;
 
-
-    // data2 = (Data *)malloc(sizeof(Data)); // 24 bytes
     type = 1;
     data2.data = &data;
     data2.type = type;
@@ -35,6 +33,5 @@ int main() {
 
     printf("union_value : %s\n", data2.data->str);
     printf("type : %d\n", data2.type);
-    // free(data2);
     return 0;
 }
