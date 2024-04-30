513ffcb668c2f7fbf1e1fc58e80d327c6f417bc2
Flynnstones <filocasoni1994@gmail.com>
2024-04-30 20:53:37 +0200

    Aggiunta commenti al codice

--- linked_list functions/union_vs_struct.c
+++ linked_list functions/union_vs_struct.c
@@ -1,22 +1,26 @@
 #include <stdio.h>  
-#include <string.h> 
+#include <string.h>
+#include <stdlib.h>
 
 typedef union Data {
-    int i;
-    float f;
-    char str[20];
-} data;
+    int i; // 4 bytes
+    float f; // 4 bytes
+    char str[20]; // 20 bytes
+} data; // 20 bytes
 
 typedef struct{
-    union Data *data;
-    int type;
-} Data;
+    union Data *data; // 20 bytes
+    int type; // 4 bytes
+} Data; // 24 bytes
 
 int main() {
     data data;
-    Data data2;
-    int type = 1;
+    Data data2; // Data *data2;
+    int type;
 
+
+    // data2 = (Data *)malloc(sizeof(Data)); // 24 bytes
+    type = 1;
     data2.data = &data;
     data2.type = type;
 
@@ -31,5 +35,6 @@ int main() {
 
     printf("union_value : %s\n", data2.data->str);
     printf("type : %d\n", data2.type);
+    // free(data2);
     return 0;
 }
