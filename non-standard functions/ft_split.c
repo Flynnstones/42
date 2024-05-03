616edd732577e96822dc8fbd6811b916a8820642
Flynnstones <filocasoni1994@gmail.com>
2024-05-03 12:16:25 +0200

    Aggiornamento ft_split.c

--- non-standard functions/ft_split.c
+++ non-standard functions/ft_split.c
@@ -2,44 +2,120 @@
 #include <stdlib.h>
 #include <string.h>
 
-char    **ft_split(char const *s, char c)
+static void	ft_freeup(char *strs)
 {
-    size_t  i;
-    size_t  j;
-    size_t  k;
-    char    **arr;
+	int	i;
 
-    arr = (char **)malloc(sizeof(char *) * (strlen(s) + 1));
-    if (!arr)
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (s[i])
-    {
-        while (s[i] == c)
-            i++;
-        if (!s[i])
-            break ;
-        k = 0;
-        while (s[i + k] && s[i + k] != c)
-            k++;
-        arr[j] = (char *)malloc(k + 1);
-        if (!arr[j])
-            return (NULL);
-        memcpy(arr[j], s + i, k);
-        arr[j][k] = '\0';
-        j++;
-        i += k;
-    }
-    arr[j] = NULL;
-    return (arr);
+	i = 0;
+	while (strs[i] != '\0')
+	{
+		free(strs);
+		i++;
+	}
+	free(strs);
 }
 
-int main()
+static int	ft_wordcount(char *str, char c)
 {
-    char *s = "Hello, World!";
-    char **arr = ft_split(s, ' ');
-    for (size_t i = 0; arr[i]; i++)
-        printf("'%s'\n", arr[i]);
-    return 0;
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != c)
+		{
+			word++;
+			while (str[i] != c && str[i] != '\0')
+				i++;
+			if (str[i] == '\0')
+				return (word);
+		}
+		i++;
+	}
+	return (word);
+}
+
+static void	ft_strcpy(char *word, char *str, char c, int j)
+{
+	int	i;
+
+	i = 0;
+	while (str[j] != '\0' && str[j] == c)
+		j++;
+	while (str[j + i] != c && str[j + i] != '\0')
+	{
+		word[i] = str[j + i];
+		i++;
+	}
+	word[i] = '\0';
+}
+
+static char	*ft_stralloc(char *str, char c, int *k)
+{
+	char	*word;
+	int		j;
+
+	j = *k;
+	word = NULL;
+	while (str[*k] != '\0')
+	{
+		if (str[*k] != c)
+		{
+			while (str[*k] != '\0' && str[*k] != c)
+				*k += 1;
+			word = (char *)malloc(sizeof(char) * (*k + 1));
+			if (word == NULL)
+				return (NULL);
+			break ;
+		}
+		*k += 1;
+	}
+	ft_strcpy(word, str, c, j);
+	return (word);
+}
+
+char	**ft_split(char const *str, char c)
+{
+	char	**strs;
+	int		i;
+	int		j;
+	int		pos;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	pos = 0;
+	j = ft_wordcount((char *)str, c);
+	strs = (char **)malloc(sizeof(char *) * (j + 1));
+	if (strs == NULL)
+		return (NULL);
+	strs[j] = NULL;
+	while (i < j)
+	{
+		strs[i] = ft_stralloc(((char *)str), c, &pos);
+		if (strs[i] == NULL)
+		{
+			ft_freeup(strs[i]);
+		}
+		i++;
+	}
+	return (strs);
+}
+
+int main(void)
+{
+    char    *str = "Hello,world,how,are,you";
+    char    **strs;
+    int     i;
+
+    strs = ft_split(str, ',');
+    i = 0;
+    while (strs[i] != NULL)
+    {
+        printf("%s\n", strs[i]);
+        i++;
+    }
+    return (0);
 }
